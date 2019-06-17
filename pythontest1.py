ea4f14e190b3e1707e7e9674e419d100c2667488
soniathomasetest <51735385+soniathomasetest@users.noreply.github.com>
2019-06-17 11:59:18 +0530

    Add files via upload

---
+++ pythontest1.py
@@ -0,0 +1,14 @@
+name=input ("what is your name?")
+print (type (name))
+print ("hello" + name)
+age=input ("What is your age")
+print (type (age))
+print (name + " age is " + age)
+print 10+int(age)
+
+"""
+print type(b)
+sum=a+b
+print a
+print b"""
+
